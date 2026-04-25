24022cb1ddf3b25ccb4cfd5a47d5495b22f3eb87
kush1410-coder <kushvarshney17@gmail.com>
2026-04-25 11:29:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.listen(5000,()=>console.log("Server running on port 5000") );
 // ✅ Routes
 app.use('/compare', compareRoute);
 app.use('/search', searchRoute);
